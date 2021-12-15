b1ae05a8e454938269e10b419d6af4ab70a893fa
bada0705 <bada0705@naver.com>
2021-12-15 14:22:44 +0900

    commit

---
+++ WebContent/WebContent/03_JAVASCRIPT/chapter01_make_script/example.js
@@ -0,0 +1,12 @@
+document.write("<h1>Welcome to javascript2</h1>");
+document.write("hello javascript1<br/>");
+document.write("hello javascript2<br/>");
+document.write("hello javascript3<br/>");
+document.write("<hr>");
+
+console.log("hello javascript1");
+console.log("hello javascript2");
+console.log("hello javascript3");
+
+alert("hello javascript!");
+
